8b048acbca1ceb390af6e4c40c277ce29bf873ec
rafaelmarviila <rafaelmarviila@gmail.com>
2019-07-08 14:25:38 -0300

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,5 @@
+numero_multiplicado = input("Por favor digite o número que deseje a ser multiplicado: ")
+
+resultado_multiplicacao = int(float(numero_multiplicado) * 2)
+
+result = print("O valor de sua multiplicação é: ", resultado_multiplicacao)
